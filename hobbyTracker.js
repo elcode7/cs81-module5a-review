703e8b14b38d6b1de20ec6227fa5e3e0a575cf3a
elcode7 <101850419+elcode7@users.noreply.github.com>
2026-01-19 20:51:10 -0800

    Add log for focused sessions count

--- hobbyTracker.js
+++ hobbyTracker.js
@@ -39,3 +39,4 @@ console.log("Total time spent:", totalTime(hobbyLog), "minutes");
 console.log("Unique hobbies:", uniqueHobbies(hobbyLog));
 console.log("Sessions longer than 30 min:", longSessions(hobbyLog, 30));
 console.log("Number of relaxed sessions:", countMood(hobbyLog, "relaxed"));
+console.log("Focused sessions:", countMood(hobbyLog, "focused"));
